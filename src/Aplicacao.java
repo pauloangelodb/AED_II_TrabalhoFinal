909b5eb7100e4c8bf4ac8dfab3b205a614afb28a
wesleymou <42716264+wesleymou@users.noreply.github.com>
2019-05-20 16:23:57 -0300

    Funcoes de localizar, adicionar, excluir e alterar

--- src/Aplicacao.java
+++ src/Aplicacao.java
@@ -1,31 +1,154 @@
-import java.io.*;
+
 import java.util.Scanner;
+
+import javax.swing.JOptionPane;
 
 public class Aplicacao {
 
-public static void main(String[] args) throws IOException {
-	ArvorePessoas lista = new ArvorePessoas("dados.txt");
-	Scanner ler = new Scanner(System.in);
-	System.out.println("tecla");
-	String a = ler.next();
-	ler.close();
-	System.out.println("pos tecla");
-	lista.escreverArquivo("dados.txt");
-	
-	
-//		CelulaPessoa novaPessoa;
-//		ArvorePessoas pessoas = new ArvorePessoas();
-//		
-//		String dados[] = new String [7];
-//		String pessoa;
-//		ArquivoTextoLeitura censo = new ArquivoTextoLeitura();
-//
-//		censo.abrirArquivo("dados ibge pequeno.txt");
-//		
-//		while (!censo.equals(null)) {
-//			pessoa = censo.ler();
-//			dados = pessoa.split(";");
-//			System.out.print(dados[1]);	
-//		}	
+	public static void main(String[] args) {
+		ArvorePessoas lista = new ArvorePessoas("dados.txt");
+		Scanner ler = new Scanner(System.in);
+		int key = 0;
+		while (key != -1) {
+			try {
+				key = Integer.parseInt(JOptionPane.showInputDialog(null,
+						"1 - Localizar" + "\n" +
+						"2 - Incluir" + "\n" +
+						"3 - Excluir" + "\n" +
+						"4 - Alterar" + "\n" +
+						"0 - Sair" + "\n"
+						, "Menu", 1));
+			} catch (NumberFormatException e) {
+				lista.escreverArquivo();
+				key = -1;
+			}
+			
+			long identidade = 0;
+			Pessoa pessoa = null;
+			switch (key) {
+			case 1:
+				try {
+					identidade = Long.parseLong(JOptionPane.showInputDialog(null, "Identidade:", "Localizar", 1));
+				} catch (NumberFormatException e) {
+				}
+				pessoa = lista.localizar(identidade);
+				if (pessoa == null) {
+					JOptionPane.showMessageDialog(null, "Não Encontrado.", null, 0);
+				}else {
+					JOptionPane.showMessageDialog(null, 
+							pessoa.getNome() + "\n" +
+							" Identidade: " + pessoa.getIdentidade() + "\n" +
+							"Sexo: " + pessoa.getSexo() + "\n" +
+							"Idade: " + pessoa.getIdade() + "\n" +
+							"Moradia: " + pessoa.getMoradia() + "\n" +
+							"Estado Civil: " + pessoa.getEstadoCivil() + "\n" +
+							"Cor: " + pessoa.getCor()
+							, null, 1);
+				}
+				break;
+
+			case 2:
+				lista.adicionar(new Pessoa(
+						Long.parseLong(JOptionPane.showInputDialog(null, "Identidade:", "Criar Cadastro", 1)),
+						JOptionPane.showInputDialog(null, "Nome:", "Criar Cadastro", 1),
+						JOptionPane.showInputDialog(null, "Sexo (M/F):", "Criar Cadastro", 1).charAt(0),
+						Integer.parseInt(JOptionPane.showInputDialog(null, "Idade:", "Criar Cadastro", 1)),
+						JOptionPane.showInputDialog(null, "Moradia:", "Criar Cadastro", 1),
+						JOptionPane.showInputDialog(null, "Estado Civil:", "Criar Cadastro", 1),
+						JOptionPane.showInputDialog(null, "Cor:", "Criar Cadastro", 1)));
+				lista.escreverArquivo();
+				break;
+				
+			case 3:
+				lista.remover(Long.parseLong(JOptionPane.showInputDialog(null, "Identidade:", "Excluir Cadastro", 1)));
+				lista.escreverArquivo();
+				break;
+				
+			case 4:
+				try {
+					identidade = Long.parseLong(JOptionPane.showInputDialog(null, "Identidade:", "Localizar", 1));
+				} catch (NumberFormatException e) {
+				}
+				pessoa = lista.localizar(identidade);
+				if (pessoa == null) {
+					JOptionPane.showMessageDialog(null, "Não Encontrado.", null, 0);
+				}else {
+					int keyInterno = 0;
+					try {
+						keyInterno = Integer.parseInt(JOptionPane.showInputDialog(null,
+								"1 - Identidade" + "\n" +
+								"2 - Nome" + "\n" +
+								"3 - Sexo" + "\n" +
+								"4 - Idade" + "\n" +
+								"5 - Moradia" + "\n" +
+								"6 - Estado Civil" + "\n" +
+								"7 - Cor" + "\n"
+								, "Alterações", 1));
+					} catch (NumberFormatException e) {
+					}
+					switch (keyInterno) {
+					case 1:
+						pessoa.setIdentidade(Integer.parseInt(JOptionPane.showInputDialog(null, "Nova Identidade:")));
+						break;
+					case 2:
+						pessoa.setNome(JOptionPane.showInputDialog(null, "Novo Nome:"));
+						break;
+					case 3:
+						pessoa.setSexo(JOptionPane.showInputDialog(null, "Sexo(M/F):").charAt(0));
+						break;
+					case 4:
+						pessoa.setIdade(Integer.parseInt(JOptionPane.showInputDialog(null, "Idade:")));
+						break;
+					case 5:
+						pessoa.setMoradia(JOptionPane.showInputDialog(null, "Moradia:"));
+						break;
+					case 6:
+						pessoa.setEstadoCivil(JOptionPane.showInputDialog(null, "Estado Civil:"));
+						break;
+					case 7:
+						pessoa.setCor(JOptionPane.showInputDialog(null, "Cor:"));
+						break;
+
+					default:
+						break;
+					}
+					JOptionPane.showMessageDialog(null, 
+							pessoa.getNome() + "\n" +
+							" Identidade: " + pessoa.getIdentidade() + "\n" +
+							"Sexo: " + pessoa.getSexo() + "\n" +
+							"Idade: " + pessoa.getIdade() + "\n" +
+							"Moradia: " + pessoa.getMoradia() + "\n" +
+							"Estado Civil: " + pessoa.getEstadoCivil() + "\n" +
+							"Cor: " + pessoa.getCor()
+							, null, 1);
+					lista.escreverArquivo();
+				}
+				break;
+				
+			case 0:
+				key = -1;
+			break;
+			
+			default:
+				break;
+			}
+		}
+		
+		ler.close();
+
+		//		CelulaPessoa novaPessoa;
+		//		ArvorePessoas pessoas = new ArvorePessoas();
+		//		
+		//		String dados[] = new String [7];
+		//		String pessoa;
+		//		ArquivoTextoLeitura censo = new ArquivoTextoLeitura();
+		//
+		//		censo.abrirArquivo("dados ibge pequeno.txt");
+		//		
+		//		while (!censo.equals(null)) {
+		//			pessoa = censo.ler();
+		//			dados = pessoa.split(";");
+		//			System.out.print(dados[1]);	
+		//		}	
 	}
 }
--- src/ArvorePessoas.java
+++ src/ArvorePessoas.java
@@ -8,91 +8,113 @@ import java.io.IOException;
 
 public class ArvorePessoas {
 	private CelulaPessoa raiz;
+	String nomeDoArquivo;
 
-    public ArvorePessoas(String nomeDoArquivo) {
-        this.raiz = null;
-        this.lerArquivo(nomeDoArquivo);
-    }
+	public ArvorePessoas(String nomeDoArquivo) {
+		this.raiz = null;
+		this.nomeDoArquivo = nomeDoArquivo;
+		this.lerArquivo(nomeDoArquivo);
+	}
 
-    public Boolean arvoreVazia() {
-        if (this.raiz == null)
-            return true;
-        else
-            return false;
-    }
+	public Boolean arvoreVazia() {
+		if (this.raiz == null)
+			return true;
+		else
+			return false;
+	}
 
-    private CelulaPessoa adicionar(CelulaPessoa raizArvore, Pessoa pessoaNova) {
-        if (raizArvore == null)
-            raizArvore = new CelulaPessoa(pessoaNova);
-        else
-        {
-            if (raizArvore.pessoa.getIdentidade() > pessoaNova.getIdentidade())
-                raizArvore.esquerda = adicionar(raizArvore.esquerda, pessoaNova);
-            else
-            {
-                if (raizArvore.pessoa.getIdentidade() < pessoaNova.getIdentidade())
-                    raizArvore.direita = adicionar(raizArvore.direita, pessoaNova);
-                else
-                    System.out.println("A pessoa " + pessoaNova.getNome() + " (identidade " + pessoaNova.getIdentidade() + ") jï¿½ foi inserida anteriormente na ï¿½rvore.");
-            }
-        }
-        return raizArvore;
-    }
-    
-    public void adicionar(Pessoa pessoaNova) {
-        this.raiz = adicionar(this.raiz, pessoaNova);
-    }
+	private CelulaPessoa adicionar(CelulaPessoa raizArvore, Pessoa pessoaNova) {
+		if (raizArvore == null)
+			raizArvore = new CelulaPessoa(pessoaNova);
+		else
+		{
+			if (raizArvore.pessoa.getIdentidade() > pessoaNova.getIdentidade())
+				raizArvore.esquerda = adicionar(raizArvore.esquerda, pessoaNova);
+			else
+			{
+				if (raizArvore.pessoa.getIdentidade() < pessoaNova.getIdentidade())
+					raizArvore.direita = adicionar(raizArvore.direita, pessoaNova);
+				else
+					System.out.println("A pessoa " + pessoaNova.getNome() + " (identidade " + pessoaNova.getIdentidade() + ") jï¿½ foi inserida anteriormente na ï¿½rvore.");
+			}
+		}
+		return raizArvore;
+	}
 
-    private CelulaPessoa antecessor(CelulaPessoa pessoaRetirar, CelulaPessoa raizArvore) {
-        if (raizArvore.direita != null) {
-            raizArvore.direita = antecessor(pessoaRetirar, raizArvore.direita);
-            return raizArvore;
-        } else {
-        	pessoaRetirar.pessoa.setIdentidade(raizArvore.pessoa.getIdentidade());
-        	pessoaRetirar.pessoa.setNome(raizArvore.pessoa.getNome());
-        	pessoaRetirar.pessoa.setSexo(raizArvore.pessoa.getSexo());
-        	pessoaRetirar.pessoa.setIdade(raizArvore.pessoa.getIdade());
-        	pessoaRetirar.pessoa.setMoradia(raizArvore.pessoa.getMoradia());
-        	pessoaRetirar.pessoa.setEstadoCivil(raizArvore.pessoa.getEstadoCivil());
-        	pessoaRetirar.pessoa.setCor(raizArvore.pessoa.getCor());
-            return raizArvore.esquerda;
-        }
-    }
+	public void adicionar(Pessoa pessoaNova) {
+		this.raiz = adicionar(this.raiz, pessoaNova);
+	}
 
-    private CelulaPessoa retirar(CelulaPessoa raizArvore, double identidade) {
-        if (raizArvore == null) {
-        	 System.out.println("a pessoa (identidade " + identidade + ") nï¿½o foi encontrada.");
-            return raizArvore;
-        } else {
-            if (raizArvore.pessoa.getIdentidade() == identidade)
-            {
-                if (raizArvore.direita == null)
-                    return (raizArvore.esquerda);
-                else {
-                    if (raizArvore.esquerda == null)
-                        return (raizArvore.direita);
-                    else {
-                        raizArvore.esquerda = antecessor(raizArvore, raizArvore.esquerda);
-                        return (raizArvore);
-                    }
-                }
-            }
-            else
-            {
-                if (raizArvore.pessoa.getIdentidade() > identidade)
-                    raizArvore.esquerda = retirar(raizArvore.esquerda, identidade);
-                else
-                    raizArvore.direita = retirar(raizArvore.direita, identidade);
-                return raizArvore;
-            }
-        }
-    }
-    
-    public void remover(double identidadeRemover) {
-        this.raiz = retirar(this.raiz, identidadeRemover);
-    }
-    
-    private void lerArquivo(String nomeArquivo) {
+	private CelulaPessoa antecessor(CelulaPessoa pessoaRetirar, CelulaPessoa raizArvore) {
+		if (raizArvore.direita != null) {
+			raizArvore.direita = antecessor(pessoaRetirar, raizArvore.direita);
+			return raizArvore;
+		} else {
+			pessoaRetirar.pessoa.setIdentidade(raizArvore.pessoa.getIdentidade());
+			pessoaRetirar.pessoa.setNome(raizArvore.pessoa.getNome());
+			pessoaRetirar.pessoa.setSexo(raizArvore.pessoa.getSexo());
+			pessoaRetirar.pessoa.setIdade(raizArvore.pessoa.getIdade());
+			pessoaRetirar.pessoa.setMoradia(raizArvore.pessoa.getMoradia());
+			pessoaRetirar.pessoa.setEstadoCivil(raizArvore.pessoa.getEstadoCivil());
+			pessoaRetirar.pessoa.setCor(raizArvore.pessoa.getCor());
+			return raizArvore.esquerda;
+		}
+	}
+
+	private CelulaPessoa remover(CelulaPessoa raizArvore, double identidade) {
+		if (raizArvore == null) {
+			System.out.println("a pessoa (identidade " + identidade + ") nï¿½o foi encontrada.");
+			return raizArvore;
+		} else {
+			if (raizArvore.pessoa.getIdentidade() == identidade)
+			{
+				if (raizArvore.direita == null)
+					return (raizArvore.esquerda);
+				else {
+					if (raizArvore.esquerda == null)
+						return (raizArvore.direita);
+					else {
+						raizArvore.esquerda = antecessor(raizArvore, raizArvore.esquerda);
+						return (raizArvore);
+					}
+				}
+			}
+			else
+			{
+				if (raizArvore.pessoa.getIdentidade() > identidade)
+					raizArvore.esquerda = remover(raizArvore.esquerda, identidade);
+				else
+					raizArvore.direita = remover(raizArvore.direita, identidade);
+				return raizArvore;
+			}
+		}
+	}
+
+	public void remover(double identidadeRemover) {
+		this.raiz = remover(this.raiz, identidadeRemover);
+	}
+
+	private Pessoa localizar(CelulaPessoa celula, long identidade) {
+		if (celula == null) {
+			return null;
+		}
+		if (celula.pessoa.getIdentidade() == identidade) {
+			return celula.pessoa;
+		}
+		else if (identidade < celula.pessoa.getIdentidade()) {
+			return this.localizar(celula.esquerda, identidade);
+		}
+		else if (identidade > celula.pessoa.getIdentidade()) {
+			return this.localizar(celula.direita, identidade);
+		}
+		return null;
+	}
+
+	public Pessoa localizar(long identidade) {
+		return this.localizar(this.raiz, identidade);
+	}
+
+	private void lerArquivo(String nomeArquivo) {
 		try (BufferedReader entrada = new BufferedReader(new FileReader(nomeArquivo))){
 			String dados = "";
 			while((dados = entrada.readLine()) != null) {
@@ -114,34 +136,38 @@ public class ArvorePessoas {
 		}
 	}
 
-    public void escreverArquivo(String nomeArquivo) {
-    	File file = new File(nomeArquivo);
-    	if(file.exists())
-    		file.delete();
-    	percorrerEmOrdem(this.raiz, nomeArquivo);
-    }
+	public void escreverArquivo() {
+		this.escreverArquivo(this.nomeDoArquivo);
+	}
 	
+	public void escreverArquivo(String nomeArquivo) {
+		File file = new File(nomeArquivo);
+		if(file.exists())
+			file.delete();
+		percorrerEmOrdem(this.raiz, nomeArquivo);
+	}
+
 	private void percorrerEmOrdem(CelulaPessoa celula, String nomeArquivo) {
 		if (celula.esquerda != null) {
-        	percorrerEmOrdem(celula.esquerda, nomeArquivo);
-        }
+			percorrerEmOrdem(celula.esquerda, nomeArquivo);
+		}
 		if (celula != null) {
-        	try (BufferedWriter saida = new BufferedWriter(new FileWriter(nomeArquivo, true))){
-        		saida.write(Long.toString(celula.pessoa.getIdentidade()) + ";");
-        		saida.write(celula.pessoa.getNome() + ";");
-        		saida.write(celula.pessoa.getSexo() + ";");
-        		saida.write(celula.pessoa.getIdade() + ";");
-        		saida.write(celula.pessoa.getMoradia() + ";");
-        		saida.write(celula.pessoa.getEstadoCivil() + ";");
-        		saida.write(celula.pessoa.getCor() + "\n");
-    		} catch (FileNotFoundException excecao) {
-    			System.out.println("Arquivo não encontrado.");
-    		} catch (IOException excecao) {
-    			System.out.println("Erro no fechamento do arquivo de escrita: " + excecao);	
-    		}
-        }
-        if (celula.direita != null) {
-        	percorrerEmOrdem(celula.direita, nomeArquivo);
-        }
-    }
+			try (BufferedWriter saida = new BufferedWriter(new FileWriter(nomeArquivo, true))){
+				saida.write(Long.toString(celula.pessoa.getIdentidade()) + ";");
+				saida.write(celula.pessoa.getNome() + ";");
+				saida.write(celula.pessoa.getSexo() + ";");
+				saida.write(celula.pessoa.getIdade() + ";");
+				saida.write(celula.pessoa.getMoradia() + ";");
+				saida.write(celula.pessoa.getEstadoCivil() + ";");
+				saida.write(celula.pessoa.getCor() + "\n");
+			} catch (FileNotFoundException excecao) {
+				System.out.println("Arquivo não encontrado.");
+			} catch (IOException excecao) {
+				System.out.println("Erro no fechamento do arquivo de escrita: " + excecao);	
+			}
+		}
+		if (celula.direita != null) {
+			percorrerEmOrdem(celula.direita, nomeArquivo);
+		}
+	}
 }
